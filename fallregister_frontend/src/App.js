1219f87b39707ca1e02726f661e2313c02221e93
j <m@m.com>
2020-03-20 22:29:54 +0000

    birthday

--- fallregister_frontend/src/App.js
+++ fallregister_frontend/src/App.js
@@ -6,11 +6,20 @@ function App() {
     <div className="App">
       <header className="App-header">
       <form action="/action_page.php">
-        <p>Patientendaten</p>
+        <h>Patientendaten</h>
+        <br></br>
+        <br></br>
   <label for="fname">First name:</label>
   <input type="text" id="fname" name="fname"></input>
+  <br></br>
   <label for="lname">Last name:</label>
   <input type="text" id="lname" name="lname"></input>
+  <br></br>
+  <label for="birthday">Geburtsdatum:</label>
+  <input type="text" id="birthday" name="birthday"></input>
+  <br></br>
+  <br></br>
+  
   <input type="submit" value="Submit"></input>
 </form>
       </header>
