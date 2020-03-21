ad495c1a593f1b7ed8c5d2603d53f69a46e80712
Simon Höninger <uodim@student.kit.edu>
2020-03-21 19:06:41 +0100

    menu

--- fallregister_frontend/src/App.js
+++ fallregister_frontend/src/App.js
@@ -9,21 +9,30 @@ import LoginForm from "./LoginForm"
 class App extends React.Component {
     constructor(){
         super();
-        this.state = {buttontext: "csvimport"};
-        this.onButtonClick = this.onButtonClick.bind(this);
+        this.state = {buttontext: "manual"};
+        this.goToManual = this.goToManual.bind(this);
+        this.goToCsvImport = this.goToCsvImport.bind(this);
+        this.goToView = this.goToView.bind(this);
     }
 
-    onButtonClick(){
-        if(this.state.buttontext === "csvimport"){
-            this.setState({buttontext: "manueller input"});
-        }else{
-            this.setState({buttontext: "csvimport"});
+    goToManual(){
+        this.setState({buttontext: "manual"});
         }
+
+    goToCsvImport(){
+        this.setState({buttontext: "csvimport"});
     }
+
+    goToView(){
+        this.setState({buttontext: "view"});
+    }
+
     render(){
         let form= <h>oh oh</h>;
-        if(this.state.buttontext === "csvimport"){
+        if(this.state.buttontext === "manual"){
             form = <PatientDataForm />;
+        }else if(this.state.buttontext === "csvimport"){
+            form = <CSVImportForm />;
         }else{
             form = <LoginForm />
         }
@@ -32,7 +41,13 @@ class App extends React.Component {
             <div id="App">
                 <header className="App-header">
                     <div class='alignleft'>
-                        <Button onClick={this.onButtonClick}>{this.state.buttontext}</Button>
+                        <Button onClick={this.goToManual}>Manueller Input</Button>
+                    </div>
+                    <div class='alignleft'>
+                    <Button onClick={this.goToCsvImport}>CSV Import</Button>
+                    </div>
+                    <div class='alignleft'>
+                    <Button onClick={this.goToView}>Datenansicht</Button>
                     </div>
                     <div class='alignright'>
                         <Button onClick={this.Logout}>Logout</Button>
