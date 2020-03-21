d0d65add489eaf7547718b52401c37065131d34d
Simon Höninger <uodim@student.kit.edu>
2020-03-21 14:24:10 +0100

    CVS Import Form

--- fallregister_frontend/src/CVSImportForm.js
+++ fallregister_frontend/src/CVSImportForm.js
@@ -1,5 +1,35 @@
-export class CVSImportForm extends React.Component {
+
+import React from 'react';
+
+import {FormLabel, Input, Grid} from '@material-ui/core/';
+
+class CVSImportForm extends React.Component {
     render(){
-        return  <h1>Hello World</h1>;
+
+        return(
+            <form action="/action_page.php">
+            <Grid
+        container
+        direction='column'
+        justify="center"
+            //alignItems='flex-start'
+            >
+
+            <Grid>
+            <h>CVS Import</h>
+            </Grid>
+
+            <Grid>
+            <FormLabel for="lname">CVS Dokument hochladen: </FormLabel>
+            <Input type="file" id="cvsFile" name="cvsFile"></Input>
+
+            </Grid>
+
+            <Input type="submit" value="Submit"></Input>
+            </Grid>
+            </form>
+    );
     }
 }
+
+export default CVSImportForm;
