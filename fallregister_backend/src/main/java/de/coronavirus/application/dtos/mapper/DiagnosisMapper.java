b9da2668127dc70944da75ade5503c94fc128b8a
Diana Burkart <diana.burkart@student.kit.edu>
2020-03-22 00:31:39 +0100

    add mapper functions and adjust dtos and responses

--- fallregister_backend/src/main/java/de/coronavirus/application/dtos/mapper/DiagnosisMapper.java
+++ fallregister_backend/src/main/java/de/coronavirus/application/dtos/mapper/DiagnosisMapper.java
@@ -3,10 +3,32 @@ package de.coronavirus.application.dtos.mapper;
 import de.coronavirus.application.dtos.response.DiagnosisResponse;
 import de.coronavirus.application.dtos.service.DiagnosisDto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DiagnosisMapper {
 
-    DiagnosisResponse toResponse(DiagnosisDto diagnosisDto) {return null;}
-    List<DiagnosisResponse> toResponseList(List<DiagnosisDto> diagnosisDtoList){return null;}
+    public static DiagnosisResponse toResponse(DiagnosisDto diagnosisDto) {
+        return diagnosisDtoToResponse(diagnosisDto);
+    }
+
+    public static List<DiagnosisResponse> toResponseList(List<DiagnosisDto> diagnosisDtoList){
+        List<DiagnosisResponse> diagnosisResponseList = new ArrayList<>();
+        for(DiagnosisDto diagnosisDto : diagnosisDtoList){
+            diagnosisResponseList.add(diagnosisDtoToResponse(diagnosisDto));
+        }
+        return diagnosisResponseList;
+    }
+
+    private static DiagnosisResponse diagnosisDtoToResponse(DiagnosisDto diagnosisDto){
+        DiagnosisResponse diagnosisResponse = new DiagnosisResponse();
+        diagnosisResponse.setDiagnosticResult(diagnosisDto.getDiagnosticResult());
+        diagnosisResponse.setDate(diagnosisDto.getDate());
+        diagnosisResponse.setLaboratoryName(diagnosisDto.getLaboratory().getName());
+        diagnosisResponse.setConfirmed(diagnosisDto.isConfirmed());
+        diagnosisResponse.setSuspicion(diagnosisDto.isSuspicion());
+        diagnosisResponse.setClinicalDiagnosis(diagnosisDto.isClinicalDiagnosis());
+        diagnosisResponse.setDead(diagnosisDto.isDead());
+        return diagnosisResponse;
+    }
 }
--- fallregister_backend/src/main/java/de/coronavirus/application/dtos/mapper/PhoneNumberMapper.java
+++ fallregister_backend/src/main/java/de/coronavirus/application/dtos/mapper/PhoneNumberMapper.java
@@ -3,10 +3,23 @@ package de.coronavirus.application.dtos.mapper;
 import de.coronavirus.application.dtos.response.PhoneNumberResponse;
 import de.coronavirus.application.dtos.service.PhoneNumberDto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PhoneNumberMapper {
 
-    PhoneNumberResponse toResponse(PhoneNumberDto phoneNumberDto){return null;};
-    List<PhoneNumberResponse> toResponseList(List<PhoneNumberDto> phoneNumberDtoList){return null;};
+    PhoneNumberResponse toResponse(PhoneNumberDto phoneNumberDto){
+        PhoneNumberResponse phoneNumberResponse = new PhoneNumberResponse();
+        phoneNumberResponse.getNumbers().add(phoneNumberDto.getNumber());
+        return phoneNumberResponse;
+    }
+
+    List<PhoneNumberResponse> toResponseList(List<PhoneNumberDto> phoneNumberDtoList){
+        List<PhoneNumberResponse> phoneNumberResponseList = new ArrayList<>();
+        for(PhoneNumberDto phoneNumberDto : phoneNumberDtoList){
+            PhoneNumberResponse phoneNumberResponse = new PhoneNumberResponse();
+            phoneNumberResponse.getNumbers().add(phoneNumberDto.getNumber());
+        }
+        return phoneNumberResponseList;
+    }
 }
