5754745c66c2d695e01f1c70bc656fbd10cb38e9
j <m@m.com>
2020-03-21 10:06:11 +0000

    first half grid

--- fallregister_frontend/src/App.js
+++ fallregister_frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 //import './App.css';
 import './App_light.css';
-import {FormLabel, Input} from '@material-ui/core/';
+import {FormLabel, Input, Grid} from '@material-ui/core/';
 
 function App() {
     return (
@@ -10,116 +10,140 @@ function App() {
                 <form action="/action_page.php">
 
                     <h>Patientendaten</h>
-                    <br></br>
+                    
+                    <Grid
+                      container
+                      direction='column'
+                      justify="center"
+                      //alignItems='center'
+                      >
+                    <Grid>
                     <FormLabel for="fname">Vorname:</FormLabel>
-                    <br></br><Input type="text" id="fname" name="fname"></Input>
-                    <br></br>
+                    <Input type="text" id="fname" name="fname"></Input>
+                    </Grid>
+                    <Grid>
                     <FormLabel for="lname">Nachname:</FormLabel>
-                    <br></br><Input type="text" id="lname" name="lname"></Input>
-                    <br></br>
+                    <Input type="text" id="lname" name="lname"></Input>
+                    </Grid>
+                    <Grid>
                     <FormLabel htmlFor="gender">Geschlecht:</FormLabel>
-                    <br></br>
+                 
                     <select id="gender" name="gender">
                     <option value="" disabled selected>Select your option</option>
                     <option value="male">Männlich</option>
                     <option value="female">Weiblich</option>
                     <option value="other">Sonstiges</option>
-                    </select><br></br>
+                    </select>
+                    </Grid>
+                    <Grid>
                     <FormLabel for="birthday">Geburtsdatum:</FormLabel>
-                    <br></br><Input type="text" id="birthday" name="birthday"></Input>
-                    <br></br>
+                    <Input type="text" id="birthday" name="birthday"></Input>
+                    </Grid>
+                    <Grid>
                     <FormLabel htmlFor="address">Adresse:</FormLabel>
-                    <br></br><Input type="text" id="address" name="address"></Input>
-                    <br></br>
+                    <Input type="text" id="address" name="address"></Input>
+                    </Grid>
+                    <Grid>                    
                     <FormLabel htmlFor="email">Email:</FormLabel>
-                    <br></br><Input type="text" id="email" name="email"></Input>
-                    <br></br>
+                    <Input type="text" id="email" name="email"></Input>
+                    </Grid>
+                    <Grid>
                     <FormLabel htmlFor="telephonenumber">Telefonnummer:</FormLabel>
-                    <br></br><Input type="text" id="telephonenumber" name="telephonenumber"></Input>
-                    <br></br>
+                    <Input type="text" id="telephonenumber" name="telephonenumber"></Input>
+                    </Grid>
+                    <Grid>
                     <FormLabel htmlFor="activity">Tätigkeit:</FormLabel>
-                    <br></br><Input type="text" id="activity" name="activity"></Input>
-                    <br></br>
+                    <Input type="text" id="activity" name="activity"></Input>
+                    </Grid>
+                    <Grid>
                     <FormLabel htmlFor="care">Betreuung:</FormLabel>
-                    <br></br><Input type="text" id="care" name="care"></Input>
-                    <br></br>
+                    <Input type="text" id="care" name="care"></Input>
+                    </Grid>
+                    <Grid>
                     <FormLabel htmlFor="stay">Unterbringung:</FormLabel>
-                    <br></br><Input type="text" id="stay" name="stay"></Input>
-                    <br></br>
+                    <Input type="text" id="stay" name="stay"></Input>
+                    </Grid>
+                    <Grid>
                     <FormLabel htmlFor="diagnose">Diagnose/Verdachtsdiagnose:</FormLabel>
-                    <br></br><Input type="text" id="diagnose" name="diagnose"></Input>
-                    <br></br>
+                    <Input type="text" id="diagnose" name="diagnose"></Input>
+                    </Grid>
+                    <Grid>
                     <FormLabel htmlFor="stay">Unterbringung:</FormLabel>
-                    <br></br><Input type="text" id="stay" name="stay"></Input>
-                    <br></br>
+                    <Input type="text" id="stay" name="stay"></Input>
+                    </Grid>
+                    <Grid>
                     <FormLabel htmlFor="dayOfInfection">Tag der Erkrankung:</FormLabel>
-                    <br></br><Input type="text" id="dayOfInfection" name="dayOfInfection"></Input>
-                    <br></br>
+                    <Input type="text" id="dayOfInfection" name="dayOfInfection"></Input>
+                    </Grid>
+                    <Grid>
                     <FormLabel htmlFor="dayOfDiagnose">Tag der Diagnose:</FormLabel>
-                    <br></br><Input type="text" id="dayOfDiagnose" name="dayOfDiagnose"></Input>
-                    <br></br>
+                    <Input type="text" id="dayOfDiagnose" name="dayOfDiagnose"></Input>
+                    </Grid>
+                    <Grid>
                     <FormLabel htmlFor="dayOfDeath">Tag des Todes:</FormLabel>
-                    <br></br><Input type="text" id="dayOfDeath" name="dayOfDeath"></Input>
-                    <br></br>
+                    <Input type="text" id="dayOfDeath" name="dayOfDeath"></Input>
+                    </Grid>
+                    <Grid>
                     <FormLabel htmlFor="timeslotOfInfection">Zeitraum der Infektion:</FormLabel>
-                    <br></br><Input type="text" id="timeslotOfInfection" name="timeslotOfInfection"></Input>
-                    <br></br>
+                    <Input type="text" id="timeslotOfInfection" name="timeslotOfInfection"></Input>
+                    </Grid>
+                    <Grid>
                     <FormLabel htmlFor="infectionSource">Infektionsquelle:</FormLabel>
-                    <br></br><Input type="text" id="infectionSource" name="infectionSource"></Input>
-                    <br></br>
+                    <Input type="text" id="infectionSource" name="infectionSource"></Input>
+                    </Grid>
+                    <Grid>
                     <FormLabel htmlFor="infectionPlace">Infektionsort:</FormLabel>
-                    <br></br><Input type="text" id="infectionPlace" name="infectionPlace"></Input>
-                    <br></br>
+                    <Input type="text" id="infectionPlace" name="infectionPlace"></Input>
+                    </Grid>
+                    <Grid>
                     <FormLabel htmlFor="transfer">Überweisung:</FormLabel>
-                    <br></br><Input type="text" id="transfer" name="transfer"></Input>
-                    <br></br>
+                    <Input type="text" id="transfer" name="transfer"></Input>
+                    </Grid>
+                    <Grid>
                     <FormLabel htmlFor="admission">Aufnahme:</FormLabel>
-                    <br></br><Input type="text" id="admission" name="admission"></Input>
-                    <br></br>
+                    <Input type="text" id="admission" name="admission"></Input>
+                    </Grid>
+                    <Grid>
                     <FormLabel htmlFor="dismissal">Entlassung:</FormLabel>
-                    <br></br><Input type="text" id="dismissal" name="dismissal"></Input>
-                    <br></br>
-                    <br></br>
+                    <Input type="text" id="dismissal" name="dismissal"></Input>
+                    </Grid>
+                    
 
                     <h>Labor</h>
-                    <br></br>
+                    
                     <FormLabel for="fname">First name:</FormLabel>
-                    <br></br><Input type="text" id="fname" name="fname"></Input>
-                    <br></br>
+                    <Input type="text" id="fname" name="fname"></Input>
+                    
                     <FormLabel for="lname">Last name:</FormLabel>
-                    <br></br><Input type="text" id="lname" name="lname"></Input>
-                    <br></br>
+                    <Input type="text" id="lname" name="lname"></Input>
+                    
                     <FormLabel for="birthday">Telefonnummer:</FormLabel>
-                    <br></br><Input type="text" id="telephonenumber" name="telephonenumber"></Input>
-                    <br></br>
+                    <Input type="text" id="telephonenumber" name="telephonenumber"></Input>
+                    
                     <FormLabel htmlFor="birthday">Email:</FormLabel>
-                    <br></br><Input type="text" id="email" name="email"></Input>
-                    <br></br>
+                    <Input type="text" id="email" name="email"></Input>
+                    
                     <FormLabel htmlFor="birthday">Untersuchungsstelle:</FormLabel>
-                    <br></br><Input type="text" id="labor" name="labor"></Input>
-                    <br></br>
+                    <Input type="text" id="labor" name="labor"></Input>
+                    
 
-                    <br></br>
+                    
                     <h>Melder</h>
-                    <br></br>
+                    
                     <FormLabel htmlFor="fname">First name:</FormLabel>
-                    <br></br><Input type="text" id="fname" name="fname"></Input>
-                    <br></br>
+                    <Input type="text" id="fname" name="fname"></Input>
+                    
                     <FormLabel htmlFor="lname">Last name:</FormLabel>
-                    <br></br><Input type="text" id="lname" name="lname"></Input>
-                    <br></br>
+                    <Input type="text" id="lname" name="lname"></Input>
+                    
                     <FormLabel htmlFor="birthday">Anschrift:</FormLabel>
-                    <br></br><Input type="text" id="address" name="address"></Input>
-                    <br></br>
+                    <Input type="text" id="address" name="address"></Input>
                     <FormLabel htmlFor="birthday">Email:</FormLabel>
-                    <br></br><Input type="text" id="email" name="email"></Input>
-                    <br></br>
+                    <Input type="text" id="email" name="email"></Input>                    
                     <FormLabel htmlFor="birthday">Telefonnummer:</FormLabel>
-                    <br></br><Input type="text" id="telephonnumber" name="telephonenumber"></Input>
-                    <br></br>
-
-                    <br></br><Input type="submit" value="Submit"></Input>
-                    <br></br><br></br>
+                    <Input type="text" id="telephonnumber" name="telephonenumber"></Input>
+                    <Input type="submit" value="Submit"></Input>
+                    </Grid>
                 </form>
             </header>
         </div>
