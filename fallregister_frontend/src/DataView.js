0367193c2ffe3f659a8f7fb74571aabf3acda99c
Charlotte <ch.goos@yahoo.de>
2020-03-22 15:01:32 +0100

    added patient table to DataView

--- fallregister_frontend/src/DataView.js
+++ fallregister_frontend/src/DataView.js
@@ -1,21 +1,40 @@
 
 import React from 'react';
 
-import {FormLabel, Input, Grid, Button} from '@material-ui/core/';
+import {FormLabel, Input, Grid, Button, makeStyles, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper} from '@material-ui/core/';
 
 class CSVImportForm extends React.Component {
     render(){
 
         return(
             <form action="/action_page.php">
-                <Grid container direction='column' justify="center">
-
-                    <Grid>
-                        <FormLabel for="testDataView">Es funktioniert!: </FormLabel>
-                        <Input type="text" id="testView" name="testView"></Input>
-                    </Grid>
-
-                </Grid>
+                <TableContainer component={Paper}>
+                    <Table aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Id</TableCell>
+                                <TableCell>Vorname</TableCell>
+                                <TableCell>Nachname</TableCell>
+                                <TableCell>Geburtsdatum</TableCell>
+                                <TableCell>Geschlecht</TableCell>
+                                <TableCell>Verdacht</TableCell>
+                                <TableCell>Klinische Diagnose</TableCell>
+                                <TableCell>Tod</TableCell>
+                                <TableCell>Todesdatum</TableCell>
+                                <TableCell>Erkrankungsbeginn</TableCell>
+                                <TableCell>Diagnosedatum</TableCell>
+                                <TableCell>Patient/in im medizinischen Bereich taetig</TableCell>
+                                <TableCell>Patient/in im Lebensmittelbereich taetig</TableCell>
+                                <TableCell>Patient/in wird in Gemeinschaftseinrichtung betreut</TableCell>
+                                <TableCell>Patient/in ist in Krankenhaus/Pflegeeinrichtung seit</TableCell>
+                                <TableCell>Patient/in ist auf der Intensivstation seit</TableCell>
+                                <TableCell>Name und Ort der Einrichtung</TableCell>
+                                <TableCell>Wahrscheinlicher Infektionsort</TableCell>
+                                <TableCell>Wahrscheinliche Infektionsquelle</TableCell>
+                            </TableRow>
+                        </TableHead>
+                    </Table>
+                </TableContainer>
             </form>
         );
     }
